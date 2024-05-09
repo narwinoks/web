1cc85b3ee34784c29ee008c6ae481dd91a91e6fc
narwinoks <narnowin00@gmail.com>
2024-05-09 20:54:40 +0700

    fix(info) : change tag p to <>
    - because not valid

--- src/common/components/module/home/Info.tsx
+++ src/common/components/module/home/Info.tsx
@@ -4,13 +4,13 @@ import Badge from '../../elements/Badge'
 
 const Info = () => {
     return (
-        <div>
+        <>
             <h1 className='font-medium text-2xl'>Hey, I’m Winarno 👋</h1>
-            <p className='mt-4'>
+            <div className='mt-4'>
                 I`m a Tech Enthusiast, and Software Engineer. I currently <Link href="/work" className='underline underline-offset-1'>Work</Link> as the Junior Software Enginer at <Badge image='/images/transindo.png' title='PT Transindo Data Perkasa' href='https://transindodata.com/'></Badge> ,
                 I create scalable web applications with specializing in PHP,JavaScript And Typescripts . People Please who know me usually call me <span className='font-semibold'>win</span> not  <span className='line-through'>nano</span> or <span>narno</span>
-            </p>
-        </div>
+            </div>
+        </>
     )
 }
 export default Info
