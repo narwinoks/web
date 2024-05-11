e4a03605918c390462e13f5bcd6b6f0785e347f9
narwinoks <narnowin00@gmail.com>
2024-05-11 22:13:36 +0700

    fix(navbar) : add useEffect because error rendering

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import NavbarMobile from './NavbarMobile';
 import { useWindowSize } from 'usehooks-ts';
 
 const Header = () => {
   const { width } = useWindowSize();
+  const [isMobile, setIsMobile] = useState(false);
 
-  const isMobile = width < 789;
+  useEffect(() => {
+    setIsMobile(width < 789);
+  }, [width]);
 
   return (
     <>
