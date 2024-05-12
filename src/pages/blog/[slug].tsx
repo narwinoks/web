7659487a090a1f3ced15802de36a20705d52b4b8
narwinoks <narnowin00@gmail.com>
2024-05-12 22:59:52 +0700

    feat(blog-detail) :add component single blog

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,8 +1,24 @@
+import BlogDetail from '@/common/components/module/blog/components/BlogDetail';
+import BlogDetailSkeleton from '@/common/components/skeleton/BlogDetailSkeleton';
+import { BLOG_ITEMS } from '@/common/constant/blogs';
+import { BlogProps } from '@/common/types/blog';
+import { NextSeo } from 'next-seo';
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const SingleBlog = () => {
+    const router = useRouter()
+    const slug = router.query.slug;
+    const blog = BLOG_ITEMS.find((item: BlogProps) => item.slug === slug);
+    const PAGE_TITLE = blog?.title;
     return (
-        <div>SingleBlog</div>
+
+        <section>
+            <NextSeo title={`${PAGE_TITLE} - Winarno`} />
+            {
+                blog ? <BlogDetail data={blog}></BlogDetail> : <BlogDetailSkeleton />
+            }
+        </section>
     )
 }
 
