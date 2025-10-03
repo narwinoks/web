ab64ce536f5b2b2f845416be1f984f81a65e7aac
narwinoks <narnowin195@gmail.com>
2025-10-03 21:54:14 +0700

    core(run-module): change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,10 @@ module.exports = {
       name: 'next-profile', // Nama aplikasi Anda
       script: './node_modules/next/dist/bin/next', // Jalankan Next.js secara langsung
       args: 'start -p 8000', // Berikan argumen start dan port di sini
-      // env: {
-      //   NODE_ENV: 'production',
-      // },
+      cwd: '/home/narnowin/nodevenv/app/19/',
+      env: {
+        NODE_ENV: 'production',
+      },
     },
   ],
 };
