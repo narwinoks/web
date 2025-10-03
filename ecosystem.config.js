ca39b4169c08a93348b510ba7ef777e5c972e96d
narwinoks <narnowin195@gmail.com>
2025-10-03 21:48:31 +0700

    core(run-module): add run module script

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'next-profile', // Nama aplikasi Anda
+      script: './node_modules/next/dist/bin/next', // Jalankan Next.js secara langsung
+      args: 'start -p 8001', // Berikan argumen start dan port di sini
+      // env: {
+      //   NODE_ENV: 'production',
+      // },
+    },
+  ],
+};
