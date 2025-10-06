b9bd326906c549f1723b2b51f5aaddb337d3dcac
dhek nar <83386456+narwinoks@users.noreply.github.com>
2025-10-06 08:02:33 +0700

    test(env): file check

--- check-env.js
+++ check-env.js
@@ -1,4 +1,5 @@
 console.log('--- Memeriksa Environment Variables ---');
 console.log('WAKATIME_API_KEY:', process.env.WAKATIME_API_KEY);
 console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('GITHUB TOKEN:', process.env.GITHUB_READ_USER_TOKEN_PERSONAL);
 console.log('--- Selesai ---');
