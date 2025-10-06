58269abafb16e3b124a6a87bc1adfbac7793d998
dhek nar <83386456+narwinoks@users.noreply.github.com>
2025-10-06 07:58:52 +0700

    test env

---
+++ check-env.js
@@ -0,0 +1,4 @@
+console.log('--- Memeriksa Environment Variables ---');
+console.log('WAKATIME_API_KEY:', process.env.WAKATIME_API_KEY);
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('--- Selesai ---');
